3f4984f5141e3fa3036bbda55ccb01a927d4aca2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-09-15 19:32:58 +0100

    Wait for websocket to be connected

--- src/test/java/cucumber/examples/java/websockets/NavigationStepdefs.java
+++ src/test/java/cucumber/examples/java/websockets/NavigationStepdefs.java
@@ -1,7 +1,10 @@
 package cucumber.examples.java.websockets;
 
 import cucumber.api.java.en.Given;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NavigationStepdefs {
     private final WebDriver webDriver;
@@ -11,7 +14,9 @@ public class NavigationStepdefs {
     }
 
     @Given("^I am on the front page$")
-    public void i_am_on_the_front_page() {
+    public void i_am_on_the_front_page() throws InterruptedException {
         webDriver.get("http://localhost:" + ServerHooks.PORT);
+        WebDriverWait wait = new WebDriverWait(webDriver, 1);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("celcius")));
     }
 }
