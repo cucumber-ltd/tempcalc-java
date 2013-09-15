f2165e37843a2ee319e879c96c9a1e4f094764ee
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-09-15 11:45:02 +0100

    Add autocomplete data and authorise travis to publish results

--- src/test/java/cucumber/examples/java/websockets/RunCukesTest.java
+++ src/test/java/cucumber/examples/java/websockets/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"html:target/cukes"})
+@CucumberOptions(format = {"html:target/cukes"}, dotcucumber = ".cucumber")
 public class RunCukesTest {
 }
